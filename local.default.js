c67a84862b8c735404db38dad2c5bcdd5a404ddf
mbhala <bhalakumaran.m@gmail.com>
2016-07-31 00:03:04 -0400

    add config template file

---
+++ local.default.js
@@ -0,0 +1,8 @@
+//Rename this file to local.js and add configs as needed.
+var config = {
+  slackTokens: ['sometokens', 'clienttoken2']
+}
+
+module.exports = {
+  slackTokens: config.slackTokens
+}
